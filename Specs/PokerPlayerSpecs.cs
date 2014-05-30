6e24567769eb83ffc2ba85150d77786112155445
Peter Gfader <peter@gfader.com>
2014-05-30 08:53:25 +0200

    Added 2 tests for the simple mehtods

---
+++ Specs/PokerPlayerSpecs.cs
@@ -0,0 +1,32 @@
+﻿namespace Specs
+{
+    using Nancy;
+    using Nancy.Testing;
+
+    using NUnit.Framework;
+
+    using PokerPlayerCsharp_NancyHost;
+
+    [TestFixture]
+    public class PokerPlayerSpecs
+    {
+        readonly Browser browser = new Browser(with => with.Module(new PokerPlayerModule()));
+
+        [Test]
+        public void GetCheck()
+        {
+            var response = browser.Get("/check");
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Test]
+        public void PostVersion()
+        {
+            var response = browser.Post("/version");
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+
+    }
+}
